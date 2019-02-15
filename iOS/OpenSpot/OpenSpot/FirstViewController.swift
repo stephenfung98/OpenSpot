df1f10ee4f3e48b842ed50ea14a37938b76160a7
Stephen Fung <sfung3@buffalo.edu>
2019-02-15 17:29:29 -0500

    start splash screen

---
+++ iOS/OpenSpot/OpenSpot/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  OpenSpot
+//
+//  Created by Stephen Fung on 2/15/19.
+//  Copyright © 2019 Stephen Fung. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
