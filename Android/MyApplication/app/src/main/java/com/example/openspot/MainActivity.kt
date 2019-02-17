b3b0eb4c45cf7df8a386945ef5e098aa50c3fc60
isaaclu98 <44326863+isaaclu98@users.noreply.github.com>
2019-02-17 14:57:39 -0500

    Firebase default UI

--- Android/MyApplication/app/src/main/java/com/example/openspot/MainActivity.kt
+++ Android/MyApplication/app/src/main/java/com/example/openspot/MainActivity.kt
@@ -1,13 +1,54 @@
 package com.example.openspot
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+
 
 class MainActivity : AppCompatActivity() {
 
+    private companion object {
+        var RC_SIGN_IN = 1
+    }
+
+    lateinit var callbacks : PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
     }
 }
+
+
+
+
+
+
+
