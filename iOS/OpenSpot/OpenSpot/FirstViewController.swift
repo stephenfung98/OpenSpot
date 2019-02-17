f6b1a6503d21cbbb16812a3e7a3d461adc138b87
Stephen Fung <sfung3@buffalo.edu>
2019-02-16 19:47:21 -0500

    Setup loging screen with phone number
    Driveway owners/commuters are now able to login with their phone numbers

--- iOS/OpenSpot/OpenSpot/FirstViewController.swift
+++ iOS/OpenSpot/OpenSpot/FirstViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import FirebaseUI
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, FUIAuthDelegate {
+    @IBAction func SignOut(_ sender: Any) {
+        try! Auth.auth().signOut()
+        
+        if self.storyboard != nil {
+            let authUI = FUIAuth.defaultAuthUI()
+            let authViewController = authUI!.authViewController()
+            self.present(authViewController, animated: false, completion: nil)
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        if Auth.auth().currentUser != nil {
+            //do something :D
+        } else {
+            let authUI = FUIAuth.defaultAuthUI()
+            authUI?.isSignInWithEmailHidden = true
+            FUIAuth.defaultAuthUI()?.shouldHideCancelButton = true
+            authUI?.delegate = self
+            let providers: [FUIAuthProvider] = [FUIPhoneAuth(authUI:FUIAuth.defaultAuthUI()!),]
+            
+            authUI?.providers = providers
+            let authViewController = authUI!.authViewController()
+            self.present(authViewController, animated: true, completion: nil)
+        }
+    }
+    
+    
 }
-
