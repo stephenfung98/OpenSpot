07ff1cb16a113237c91157b3b0d23b9dc7911f25
Stephen Fung <sfung3@buffalo.edu>
2019-02-27 17:28:49 -0500

    Hotfix: Fixed login not showing
    - Fixed login not showing if user is not signed in

--- iOS/OpenSpot/OpenSpot/FirstViewController.swift
+++ iOS/OpenSpot/OpenSpot/FirstViewController.swift
@@ -14,20 +14,35 @@ import CoreLocation
 class FirstViewController: UIViewController, FUIAuthDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var zoomButton: UIButton!
-    
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 1000
+    
+    @IBAction func zoomButton(_ sender: Any) {
+        centerViewOnUserLocation()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationServices()
     }
     
-    @IBAction func Press(_ sender: UIButton) {
-        centerViewOnUserLocation()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if Auth.auth().currentUser != nil {
+            //do something :D
+        } else {
+            let authUI = FUIAuth.defaultAuthUI()
+            authUI?.isSignInWithEmailHidden = true
+            FUIAuth.defaultAuthUI()?.shouldHideCancelButton = true
+            authUI?.delegate = self
+            let providers: [FUIAuthProvider] = [FUIPhoneAuth(authUI:FUIAuth.defaultAuthUI()!)]
+            
+            authUI?.providers = providers
+            let authViewController = authUI!.authViewController()
+            self.present(authViewController, animated: false, completion: nil)
+        }
     }
-
     
     func setupLocationManager() {
         locationManager.delegate = self
@@ -75,8 +90,7 @@ class FirstViewController: UIViewController, FUIAuthDelegate {
 }
 
 
-extension FirstViewController: CLLocationManagerDelegate {    
-    
+extension FirstViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthorization()
     }
