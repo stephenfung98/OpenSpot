9d3172ba0b133dc6241b014ef474587cc127990f
echiu2 <echiu2@buffalo.edu>
2019-03-10 16:45:37 -0400

    Added dropdown items for car make and create a spinner & dropdown men for car color and car year
    -Added dropdown items for car make, colors & year with spinners

--- Android/MyApplication/app/src/main/java/com/example/openspot/VehicleInfoActivity.kt
+++ Android/MyApplication/app/src/main/java/com/example/openspot/VehicleInfoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.openspot
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -9,10 +10,10 @@ class VehicleInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_vehicle_info)
 
-
-        val list = arrayListOf("Mercury","Jupiter","Venus","Earth","Saturn","Car Make")
+        val list = arrayListOf("Ford","Honda","Nissan","Toyota","Volkswagen","Car Make")
         val listSize = list.size -1
 
         val spinner: Spinner = findViewById(R.id.spinner)
@@ -25,5 +26,35 @@ class VehicleInfoActivity : AppCompatActivity() {
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = dataAdapter
         spinner.setSelection(listSize)
+
+        val list3 = arrayListOf("Black","Blue","Green","Red","White","Color")
+        val listSize3 = list.size -1
+
+        val spinner3: Spinner = findViewById(R.id.spinner3)
+
+        val dataAdapter3 = object : ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, list3) {
+            override fun getCount(): Int {
+                return listSize3 // Truncate the list
+            }
+        }
+        dataAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner3.adapter = dataAdapter3
+        spinner3.setSelection(listSize)
+
+
+        val list4 = arrayListOf("2019","2018","2017","2016","2015","Year")
+        val listSize4 = list.size -1
+
+
+        val spinner4: Spinner = findViewById(R.id.spinner4)
+
+        val dataAdapter4 = object : ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, list4) {
+            override fun getCount(): Int {
+                return listSize4 // Truncate the list
+            }
+        }
+        dataAdapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner4.adapter = dataAdapter4
+        spinner4.setSelection(listSize)
     }
 }
