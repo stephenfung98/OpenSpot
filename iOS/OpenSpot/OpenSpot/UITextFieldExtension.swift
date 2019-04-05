40b6e6f2a205b5334c9b2b1e7862461f6248d805
Jay <jay.lligui@gmail.com>
2019-04-05 03:04:41 -0400

    Code Cleanup
    -reviewed and cleaned up code

--- iOS/OpenSpot/OpenSpot/UITextFieldExtension.swift
+++ iOS/OpenSpot/OpenSpot/UITextFieldExtension.swift
@@ -7,34 +7,6 @@
 //
 import UIKit
 
-class UITextFieldExtension: UITextField {
-    
-    //    let padding = UIEdgeInsets(top: 4, left: 12, bottom: 4, right: 12)
-    //
-    //    override open func textRect(forBounds bounds: CGRect) -> CGRect {
-    //        return bounds.inset(by: padding)
-    //    }
-    //
-    //    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
-    //        return bounds.inset(by: padding)
-    //    }
-    //
-    //    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
-    //        return bounds.inset(by: padding)
-    //    }
-    //
-    //    @IBInspectable var borderColor: UIColor? {
-    //        didSet {
-    //            layer.borderColor = borderColor?.cgColor
-    //        }
-    //    }
-    //    @IBInspectable var borderWidth: CGFloat = 0 {
-    //        didSet {
-    //            layer.borderWidth = borderWidth
-    //        }
-    //    }
-}
-
 extension UITextField{
     
     @IBInspectable var doneAccessory: Bool{
